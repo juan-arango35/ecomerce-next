b7cc3cd09b31042ad57ac4b249ff725712aecc5b
juan arango <juanarangozapata35@gmail.com>
2024-11-01 11:23:40 -0500

    update products[id]

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -8,6 +8,7 @@ export default async function CategoriesPage() {
     <div className="flex flex-col md:flex-row m-2 gap-4">
       {categories.map((category) => (
         <Link
+          key={category}
           href={`/categories/${category}`}
           className="flex items-center justify-center w-full md:w-1/2 h-32 text-2xl md:text-xl font-bold text-blue-700 p-4 bg-white border rounded-lg shadow-lg hover:shadow-blue-300 transition-shadow duration-300"
         >
