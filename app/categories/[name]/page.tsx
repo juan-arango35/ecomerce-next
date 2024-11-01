fc1348558c607274df4a21d44cb92779c86b12a8
juan arango <juanarangozapata35@gmail.com>
2024-11-01 10:50:05 -0500

    agrega categories

---
+++ app/categories/[name]/page.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { Product } from "@/app/interfaces";
+import ProductCard from "@/app/components/ProductCard";
+
+interface CategoryProductsProps {
+  params: { name: string };
+}
+
+async function loadCategoryProducts(
+  category: string
+): Promise<Product[] | null> {
+  const response = await fetch(
+    `https://fakestoreapi.com/products/category/${category}`
+  );
+  if (!response.ok) {
+    return null;
+  }
+  const data: Product[] = await response.json();
+  return data;
+}
+
+const CategoryProducts = async ({ params }: CategoryProductsProps) => {
+  const categoryName = decodeURIComponent(params.name);
+  const products = await loadCategoryProducts(params.name);
+
+  if (!products) {
+    notFound();
+    return null;
+  }
+
+  return (
+    <div>
+      <h1 className="text-gray-700 mb-3 mt-3 ml-12 text-2xl font-bold ">
+        {categoryName.toUpperCase()}
+      </h1>
+      {products.length > 0 ? (
+        <ProductCard products={products} buttonText="View Details" />
+      ) : (
+        <h1 className="text-gray-700 mb-3 mt-3 ml-12 text-2xl font-bold ">
+          There are not products in this category
+        </h1>
+      )}
+    </div>
+  );
+};
+
+export default CategoryProducts;
---
+++ app/categories/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default async function CategoriesPage() {
+  const res = await fetch("https://fakestoreapi.com/products/categories");
+  const categories: string[] = await res.json();
+
+  return (
+    <div className="flex flex-col md:flex-row m-2 gap-4">
+      {categories.map((category) => (
+        <Link
+          href={`/categories/${category}`}
+          className="flex items-center justify-center w-full md:w-1/2 h-32 text-2xl md:text-xl font-bold text-blue-700 p-4 bg-white border rounded-lg shadow-lg hover:shadow-blue-300 transition-shadow duration-300"
+        >
+          {category.toUpperCase()}
+        </Link>
+      ))}
+    </div>
+  );
+}
